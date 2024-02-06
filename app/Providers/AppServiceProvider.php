d11f41409bf864958fda423b5d55a36c36b65bd0
herimaulana <herimaulana85@gmail.com>
2024-02-06 10:55:41 +0700

    untuk kebutuhan curency dan juga urk segment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Http\Request;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Untuk Kebutuhan Mata Uang Indonesia
+        Blade::directive('rp', function ( $expression ) { return "Rp. <?php echo number_format($expression,0,',','.'); ?>"; });
+        // Untuk Url Segment
+        Blade::directive('urlSegment', function ($expression) { return "<?php echo ucfirst(Request::segment($expression));  ?>"; });
     }
 }
