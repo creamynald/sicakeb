813926f835c63d4a6612ca22e3a6833c7bffa64c
herimaulana <herimaulana85@gmail.com>
2024-02-06 10:56:58 +0700

    Kebutuhan crud opd

---
+++ app/Models/opd.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class opd extends Model
+{
+    use HasFactory;
+    protected $guarded = [
+        'id'
+    ];
+}
