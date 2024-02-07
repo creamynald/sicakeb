d81a5f94d065cb5909b307264d9f2e58c46a9c1e
herimaulana <herimaulana85@gmail.com>
2024-02-07 10:35:59 +0700

    Menampilkan data dalam bentuk data table yajra

--- app/Http/Controllers/backend/opdController.php
+++ app/Http/Controllers/backend/opdController.php
@@ -4,15 +4,44 @@ namespace App\Http\Controllers\backend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use DataTables;
+use App\Models\opd;
 class opdController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
         //
+        if($request->ajax()){
+            $data = opd::latest()->get();
+            return DataTables::of($data)
+                ->addIndexColumn()
+                ->addColumn('action', function($row){
+                    $actionBtn = '
+                    <div class="d-flex justify-content-end flex-shrink-0">
+                        <a href="javascript:void(0)" onclick="openEditModal('.$row->id.')" class="btn btn-icon btn-bg-light btn-active-color-primary btn-sm me-1 modal-trigger">
+                            <i class="ki-duotone ki-pencil fs-2">
+                                <span class="path1"></span>
+                                <span class="path2"></span>
+                            </i>
+                        </a>
+                        <a href="#" class="btn btn-icon btn-bg-light btn-active-color-danger btn-sm">
+                            <i class="ki-duotone ki-trash fs-2">
+                                <span class="path1"></span>
+                                <span class="path2"></span>
+                                <span class="path3"></span>
+                                <span class="path4"></span>
+                                <span class="path5"></span>
+                            </i>
+                        </a>
+                    </div>';
+                    return $actionBtn;
+                })
+                ->rawColumns(['action'])
+                ->make(true);
+        }
         return view('backend.opd.index');
     }
 
@@ -30,6 +59,18 @@ class opdController extends Controller
     public function store(Request $request)
     {
         //
+        $request->validate([
+            'nama' => 'required',
+            'singkatan' => 'required',
+        ],
+        [
+            'nama.required' => 'Nama Objek harus diisi',
+            'singkatan.required' => 'Singkatan Penerimaan harus diisi',
+        ]);
+
+        opd::create($request->all());
+
+        return to_route('opd.index')->with('success', 'Data berhasil disimpan');
     }
 
     /**
