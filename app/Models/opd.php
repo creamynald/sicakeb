0bcd22ddc62db2389e316d167b7cd218953f20c4
herimaulana <herimaulana85@gmail.com>
2024-02-09 22:00:08 +0700

    Mendefinisikan nama tabel, fillable column dan relasi

--- app/Models/opd.php
+++ app/Models/opd.php
@@ -4,11 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Opd\Pegawai;
 
 class opd extends Model
 {
     use HasFactory;
-    protected $guarded = [
-        'id'
+    // define name of table in database
+    protected $table = 'opds';
+    
+    // begin::define column that fillable
+    protected $fillable = [
+        'nama',
+        'singkatan'
     ];
+    // end::define column that fillable
+
+    // begin::relation to pegawai model
+    public function pegawai()
+    {
+        return $this->hasMany(Pegawai::class);
+    }
+    // end::relation to pegawai model
 }
