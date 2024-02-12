65edfbc2784490176f51b7f7454eee706524bc7f
creamynald <creamynald@gmail.com>
2024-02-12 10:08:20 +0700

    fix table users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('opd_id')->nullable()->constrained('opd')->onDelete('cascade');
+            $table->string('opd_id')->nullable();
             $table->string('name');
             $table->string('email')->unique();
             $table->string('profile_photo_path', 2048)->nullable();
