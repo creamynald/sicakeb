6da490fcac226f951e9e7aed7b603f713fe3fa49
herimaulana <herimaulana85@gmail.com>
2024-02-12 15:29:23 +0700

    Kebuthan routing

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,11 @@
 use App\Http\Controllers\backend\dashboardController;
 use App\Http\Controllers\backend\opdController;
 use App\Http\Controllers\backend\opd\pegawaiController;
+use App\Http\Controllers\backend\opd\TujuanController;
 use App\Http\Controllers\backend\opd\SasaranController;
+use App\Http\Controllers\backend\opd\ProgramController;
+use App\Http\Controllers\backend\opd\KegiatanController;
+use App\Http\Controllers\backend\opd\SubkegiatanController;
 use App\Http\Controllers\backend\rolesAndPermission\rolesController;
 use Illuminate\Support\Facades\Route;
 
@@ -47,6 +51,15 @@ Route::prefix('admin')->middleware('auth')->group(function () {
     });
     // end::pegawai controller
 
+    // begin::tujuan controller that can be access by super admin and admin only
+    Route::group(['middleware' => ['role:Super-Admin|admin']], function () {
+        Route::resource('tujuan', TujuanController::class);
+        // begin:additional route (!= resource)
+        Route::post('tujuan/save', [TujuanController::class, 'saveData']);
+        // end:additional route
+    });
+    // end::tujuan controller
+
     // begin::sasaran controller that can be access by super admin and admin only
     Route::group(['middleware' => ['role:Super-Admin|admin']], function () {
         Route::resource('sasaran', SasaranController::class);
@@ -55,6 +68,33 @@ Route::prefix('admin')->middleware('auth')->group(function () {
         // end:additional route
     });
     // end::sasaran controller
+
+    // begin::program controller that can be access by super admin and admin only
+    Route::group(['middleware' => ['role:Super-Admin|admin']], function () {
+        Route::resource('program', ProgramController::class);
+        // begin:additional route (!= resource)
+        Route::post('program/save', [ProgramController::class, 'saveData']);
+        // end:additional route
+    });
+    // end::program controller
+
+    // begin::kegiatan controller that can be access by super admin and admin only
+    Route::group(['middleware' => ['role:Super-Admin|admin']], function () {
+        Route::resource('kegiatan', KegiatanController::class);
+        // begin:additional route (!= resource)
+        Route::post('kegiatan/save', [KegiatanController::class, 'saveData']);
+        // end:additional route
+    });
+    // end::kegiatan controller
+
+    // begin::subkegiatan controller that can be access by super admin and admin only
+    Route::group(['middleware' => ['role:Super-Admin|admin']], function () {
+        Route::resource('subkegiatan', SubkegiatanController::class);
+        // begin:additional route (!= resource)
+        Route::post('subkegiatan/save', [SubkegiatanController::class, 'saveData']);
+        // end:additional route
+    });
+    // end::subkegiatan controller
 
     // for super admin only
     Route::group(['middleware' => ['role:Super-Admin']], function () {
