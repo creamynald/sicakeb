8c45d55238b4c6c516f6c14fee7bb8e347e8f186
herimaulana <herimaulana85@gmail.com>
2024-02-12 15:20:31 +0700

    CRUD Program

---
+++ app/Models/Opd/Program.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models\Opd;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Opd\Sasaran;
+use App\Models\Opd\Kegiatan;
+
+class Program extends Model
+{
+    use HasFactory;
+
+    // define table name in database
+    protected $table = 'programs';
+
+    // begin::define fillable column
+    protected $fillable = [
+        'sasaran_id',
+        'nama'
+    ];
+    // end::define fillable column
+
+    // begin::relation to Sasaran model
+    public function sasaran()
+    {
+        return $this->belongsTo(Sasaran::class);
+    }
+    // end::relation to Sasaran model
+
+    // begin::relation to Kegiatan model
+    public function kegiatan()
+    {
+        return $this->hasMany(Kegiatan::class);
+    }
+    // end::relation to Kegiatan model
+}
---
+++ resources/views/backend/program/index.blade.php
@@ -0,0 +1,298 @@
+@extends('backend.layouts.base')
+{{-- This page using "demo1/apps/customers/list.html" to get the data table --}}
+@section('content')
+    <!--begin::Content wrapper-->
+    <div class="d-flex flex-column flex-column-fluid">
+        <!--begin::Toolbar-->
+        <div id="kt_app_toolbar" class="app-toolbar py-3 py-lg-6">
+            <!--begin::Toolbar container-->
+            <div id="kt_app_toolbar_container" class="app-container container-xxl d-flex flex-stack">
+                <!--begin::Page title-->
+                <div class="page-title d-flex flex-column justify-content-center flex-wrap me-3">
+                    <!--begin::Title-->
+                    <h1 class="page-heading d-flex text-gray-900 fw-bold fs-3 flex-column justify-content-center my-0">
+                        @urlSegment(2) List</h1>
+                    <!--end::Title-->
+                    <!--begin::Breadcrumb-->
+                    <ul class="breadcrumb breadcrumb-separatorless fw-semibold fs-7 my-0 pt-1">
+                        <!--begin::Item-->
+                        <li class="breadcrumb-item text-muted">
+                            <a href="{{ url('/admin/dashboard') }}" class="text-muted text-hover-primary">Dashboard</a>
+                        </li>
+                        <!--end::Item-->
+                        <!--begin::Item-->
+                        <li class="breadcrumb-item">
+                            <span class="bullet bg-gray-500 w-5px h-2px"></span>
+                        </li>
+                        <!--end::Item-->
+                        <!--begin::Item-->
+                        <li class="breadcrumb-item text-muted">@urlSegment(2)</li>
+                        <!--end::Item-->
+                    </ul>
+                    <!--end::Breadcrumb-->
+                </div>
+                <!--end::Page title-->
+                <!--begin::Actions-->
+                <div class="d-flex align-items-center gap-2 gap-lg-3">
+                    <!--begin::Filter menu-->
+                    <div class="m-0">
+                        <!--begin::Menu toggle-->
+                        {{-- <a href="#" class="btn btn-sm btn-flex btn-secondary fw-bold" data-kt-menu-trigger="click"
+                            data-kt-menu-placement="bottom-end">
+                            <i class="ki-duotone ki-filter fs-6 text-muted me-1">
+                                <span class="path1"></span>
+                                <span class="path2"></span>
+                            </i>Filter</a> --}}
+                        <!--end::Menu toggle-->
+                        <!--begin::Menu 1-->
+                        <div class="menu menu-sub menu-sub-dropdown w-250px w-md-300px" data-kt-menu="true"
+                            id="kt_menu_654c7021994de">
+                            <!--begin::Header-->
+                            <div class="px-7 py-5">
+                                <div class="fs-5 text-gray-900 fw-bold">Filter Options</div>
+                            </div>
+                            <!--end::Header-->
+                            <!--begin::Menu separator-->
+                            <div class="separator border-gray-200"></div>
+                            <!--end::Menu separator-->
+                            <!--begin::Form-->
+                            <div class="px-7 py-5">
+                                <!--begin::Input group-->
+                                <div class="mb-10">
+                                    <!--begin::Label-->
+                                    <label class="form-label fw-semibold">Status:</label>
+                                    <!--end::Label-->
+                                    <!--begin::Input-->
+                                    <div>
+                                        <select class="form-select form-select-solid" multiple="multiple"
+                                            data-kt-select2="true" data-close-on-select="false"
+                                            data-placeholder="Select option" data-dropdown-parent="#kt_menu_654c7021994de"
+                                            data-allow-clear="true">
+                                            <option></option>
+                                            <option value="1">Approved</option>
+                                            <option value="2">Pending</option>
+                                            <option value="2">In Process</option>
+                                            <option value="2">Rejected</option>
+                                        </select>
+                                    </div>
+                                    <!--end::Input-->
+                                </div>
+                                <!--end::Input group-->
+                                <!--begin::Input group-->
+                                <div class="mb-10">
+                                    <!--begin::Label-->
+                                    <label class="form-label fw-semibold">Member Type:</label>
+                                    <!--end::Label-->
+                                    <!--begin::Options-->
+                                    <div class="d-flex">
+                                        <!--begin::Options-->
+                                        <label class="form-check form-check-sm form-check-custom form-check-solid me-5">
+                                            <input class="form-check-input" type="checkbox" value="1" />
+                                            <span class="form-check-label">Author</span>
+                                        </label>
+                                        <!--end::Options-->
+                                        <!--begin::Options-->
+                                        <label class="form-check form-check-sm form-check-custom form-check-solid">
+                                            <input class="form-check-input" type="checkbox" value="2"
+                                                checked="checked" />
+                                            <span class="form-check-label">Customer</span>
+                                        </label>
+                                        <!--end::Options-->
+                                    </div>
+                                    <!--end::Options-->
+                                </div>
+                                <!--end::Input group-->
+                                <!--begin::Input group-->
+                                <div class="mb-10">
+                                    <!--begin::Label-->
+                                    <label class="form-label fw-semibold">Notifications:</label>
+                                    <!--end::Label-->
+                                    <!--begin::Switch-->
+                                    <div class="form-check form-switch form-switch-sm form-check-custom form-check-solid">
+                                        <input class="form-check-input" type="checkbox" value="" name="notifications"
+                                            checked="checked" />
+                                        <label class="form-check-label">Enabled</label>
+                                    </div>
+                                    <!--end::Switch-->
+                                </div>
+                                <!--end::Input group-->
+                                <!--begin::Actions-->
+                                <div class="d-flex justify-content-end">
+                                    <button type="reset" class="btn btn-sm btn-light btn-active-light-primary me-2"
+                                        data-kt-menu-dismiss="true">Reset</button>
+                                    <button type="submit" class="btn btn-sm btn-primary"
+                                        data-kt-menu-dismiss="true">Apply</button>
+                                </div>
+                                <!--end::Actions-->
+                            </div>
+                            <!--end::Form-->
+                        </div>
+                        <!--end::Menu 1-->
+                    </div>
+                    <!--end::Filter menu-->
+                    <!--begin::Secondary button-->
+                    <!--end::Secondary button-->
+                    <!--begin::Primary button-->
+                    {{-- <a href="#" class="btn btn-sm fw-bold btn-primary" data-bs-toggle="modal"
+                        data-bs-target="#kt_modal_create_app">Create</a> --}}
+                    <!--end::Primary button-->
+                </div>
+                <!--end::Actions-->
+            </div>
+            <!--end::Toolbar container-->
+        </div>
+        <!--end::Toolbar-->
+        <!--begin::Content-->
+        <div id="kt_app_content" class="app-content flex-column-fluid">
+            <!--begin::Content container-->
+            <div id="kt_app_content_container" class="app-container container-xxl">
+                <!--begin::Card-->
+                <div class="card">
+                    <!--begin::Card header-->
+                    <div class="card-header border-0 pt-6">
+                        <!--begin::Card title-->
+                        <div class="card-title">
+                            <!--begin::Search-->
+                            <div class="d-flex align-items-center position-relative my-1">
+                                <i class="ki-duotone ki-magnifier fs-3 position-absolute ms-5">
+                                    <span class="path1"></span>
+                                    <span class="path2"></span>
+                                </i>
+                                <div class="dataTables_filter ">
+                                    {{-- begin::pencarian manual untuk data opd --}}
+                                    <input type="text" id="search" data-kt-docs-table-filter="search" class="form-control form-control-solid w-250px ps-15" placeholder="Search.."/>
+                                    {{-- end::pencarian manual untuk data opd --}}
+                                </div>
+                            </div>
+                            <!--end::Search-->
+                        </div>
+                        <!--begin::Card title-->
+                        <!--begin::Card toolbar-->
+                        <div class="card-toolbar">
+                            <!--begin::Toolbar-->
+                            <div class="d-flex justify-content-end" data-kt-customer-table-toolbar="base">
+                                <!--begin::Add customer-->
+                                <button type="button" class="btn btn-primary btn-sm" data-bs-toggle="modal"
+                                    id="btnTambah">Tambah Data</button>
+                                <!--end::Add customer-->
+                            </div>
+                            <!--end::Toolbar-->
+                            <!--begin::Group actions-->
+                            <div class="d-flex justify-content-end align-items-center d-none"
+                                data-kt-customer-table-toolbar="selected">
+                                <div class="fw-bold me-5">
+                                    <span class="me-2" data-kt-customer-table-select="selected_count"></span>Selected
+                                </div>
+                                <button type="button" class="btn btn-danger"
+                                    data-kt-customer-table-select="delete_selected">Delete Selected</button>
+                            </div>
+                            <!--end::Group actions-->
+                        </div>
+                        <!--end::Card toolbar-->
+                    </div>
+                    <!--end::Card header-->
+                    <!--begin::Card body-->
+                    <div class="card-body pt-0">
+                        <!--begin::Table-->
+                        <table id="myTable" class="table align-middle table-row-dashed fs-6 gy-5">
+                            <thead>
+                                <tr class="text-start text-gray-500 fw-bold fs-7 text-uppercase gs-0">
+                                    <th class="min-w-30px">No</th>
+                                    <th class="min-w-30px">Sasaran</th>
+                                    <th class="min-w-125px">Program</th>
+                                    <th class="text-end min-w-70px">Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody class="fw-bold fs-6 text-gray-600">
+                            </tbody>
+                        </table>
+                        <!--end::Table-->
+                    </div>
+                    <!--end::Card body-->
+                </div>
+                <!--end::Card-->
+                <!--begin::Modals-->
+                <!-- Modal Tambah/Edit -->
+                <div class="modal fade" id="formModal" tabindex="-1" role="dialog" aria-labelledby="formModalLabel"
+                    aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="formModalLabel">Form Data @urlSegment(2)</h5>
+                            </div>
+                            <form id="formData" method="POST">
+                                <div class="modal-body">
+                                    @csrf
+                                    <input type="hidden" id="dataId" name="dataId">
+                                    <div class="form-group">
+                                        <label for="sasaran_id" class="required fs-6 fw-semibold mb-2">Sasaran</label>
+                                        <select name="sasaran_id" id="sasaran_id" class="form-select form-select-solid">
+                                            @foreach ($sasaran as $item)                                                
+                                                <option value="{{$item->id}}">{{$item->nama}}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="nama" class="required fs-6 fw-semibold mb-2">Tujuan</label>
+                                        <input type="text" class="form-control" id="nama" name="nama">
+                                    </div>
+                                </div>
+                                <!--begin::Modal footer-->
+                                <div class="modal-footer flex-center">
+                                    <!--begin::Button-->
+                                    <button type="button" id="" class="btn btn-warning me-3"
+                                        data-bs-dismiss="modal">Batal</button>
+                                    <!--end::Button-->
+                                    <!--begin::Button-->
+                                    <button type="submit" class="btn btn-primary">
+                                        <span class="indicator-label" id="btnSimpan">Simpan</span>
+                                        <span class="indicator-progress">Please wait...
+                                            <span class="spinner-border spinner-border-sm align-middle ms-2">
+                                            </span>
+                                        </span>
+                                    </button>
+                                    <!--end::Button-->
+                                </div>
+                                <!--end::Modal footer-->
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <!--end::Modals-->
+            </div>
+            <!--end::Content container-->
+        </div>
+        <!--end::Content-->
+    </div>
+    <!--end::Content wrapper-->
+
+    {{-- begin::additional css --}}
+    @push('css')
+        <!--begin::Vendor Stylesheets(used for this page only)-->
+        <link href="{{ asset('assets/plugins/custom/datatables/datatables.bundle.css') }}" rel="stylesheet"
+            type="text/css" />
+        <!--end::Vendor Stylesheets-->
+    @endpush
+    {{-- end::aditional css --}}
+    {{-- begin::additional js --}}
+    @push('js')
+        <script src="{{ asset('assets/plugins/custom/datatables/datatables.bundle.js') }}"></script>
+        <!--begin::Custom Javascript(used for this page only)-->
+        <script src="{{ asset('assets/js/custom/apps/customers/list/export.js') }}"></script>
+        <script src="{{ asset('assets/js/custom/apps/customers/list/list.js') }}"></script>
+        <script src="{{ asset('assets/js/custom/apps/customers/add.js') }}"></script>
+        <script src="{{ asset('assets/js/widgets.bundle.js') }}"></script>
+        <script src="{{ asset('assets/js/custom/widgets.js') }}"></script>
+        <script src="{{ asset('assets/js/custom/apps/chat/chat.js') }}"></script>
+        <script src="{{ asset('assets/js/custom/utilities/modals/upgrade-plan.js') }}"></script>
+        <script src="{{ asset('assets/js/custom/utilities/modals/create-app.js') }}"></script>
+        <script src="{{ asset('assets/js/custom/utilities/modals/users-search.js') }}"></script>
+        <!--end::Custom Javascript-->
+    @endpush
+    {{-- begin::custom js --}}
+    @push('scripts')
+        @include('backend.'.Request::segment(2).'.script')
+    @endpush
+    {{-- end::custom js --}}
+    {{-- end::aditional js --}}
+@endsection
