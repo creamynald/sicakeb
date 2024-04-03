74605cd069c94bd9324070bd6325c96850fdc08a
herimaulana <herimaulana85@gmail.com>
2024-04-03 11:19:31 +0700

    Custom halaman login dan menambahkan logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -81,8 +81,8 @@
                 <!--begin::Content-->
                 <div class="d-flex flex-column flex-center py-7 py-lg-15 px-5 px-md-15 w-100">
                     <!--begin::Logo-->
-                    <a href="index.html" class="mb-0 mb-lg-12">
-                        <img alt="Logo" src="assets/media/logos/custom-1.png" class="h-60px h-lg-75px" />
+                    <a href="#" class="mb-0 mb-lg-12">
+                        <img alt="Logo" src="{{asset('assets/favicon/Logo_SiCakeb.png')}}" class="h-60px h-lg-75px" />
                     </a>
                     <!--end::Logo-->
                     <!--begin::Image-->
@@ -90,17 +90,13 @@
                         src="assets/media/misc/auth-screens.png" alt="" />
                     <!--end::Image-->
                     <!--begin::Title-->
-                    <h1 class="d-none d-lg-block text-white fs-2qx fw-bolder text-center mb-7">Fast, Efficient and
-                        Productive</h1>
+                    <h1 class="d-none d-lg-block text-white fs-2qx fw-bolder text-center mb-7">Sistem Capaian Kinerja Elektronik Kabupaten Bengkalis (SICAKEB)</h1>
                     <!--end::Title-->
                     <!--begin::Text-->
-                    <div class="d-none d-lg-block text-white fs-base text-center">In this kind of post,
-                        <a href="#" class="opacity-75-hover text-warning fw-bold me-1">the blogger</a>introduces
-                        a person they’ve interviewed
-                        <br />and provides some background information about
-                        <a href="#" class="opacity-75-hover text-warning fw-bold me-1">the interviewee</a>and
-                        their
-                        <br />work following this is a transcript of the interview.
+                    <div class="d-none d-lg-block text-white fs-base text-center">Mengukur,
+                        <a href="#" class="opacity-75-hover text-warning fw-bold me-1">Capaian Kinerja</a>Kabupaten Bengkalis
+                        <br />Menuju
+                        <a href="#" class="opacity-75-hover text-warning fw-bold me-1">Bengkalis Bermasa</a>
                     </div>
                     <!--end::Text-->
                 </div>
