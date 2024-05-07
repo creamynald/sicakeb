b4a389399ae05bb5f33ad72a1d4fcaf7e665be14
creamynald <creamynald@gmail.com>
2024-05-07 09:30:34 +0700

    typo route

--- routes/web.php
+++ routes/web.php
@@ -131,7 +131,7 @@ Route::prefix('admin')
 
         Route::prefix('setting')->group(function () {
             // edit profile
-            Route::get('users/{user}/edit', [userController::class, 'editProfile'])->name('profile.edit');l
+            Route::get('users/{user}/edit', [userController::class, 'editProfile'])->name('profile.edit');
             Route::put('users/{user}/update', [UserController::class, 'updateProfile'])->name('profile.update');
 
 
